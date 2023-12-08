e89a29d4a05896927c896b197512e4853fbf11df
taishi <batapi3939@gmail.com>
2023-12-08 11:30:07 +0900

    test

---
+++ app/src/main/java/com/example/calculationapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.calculationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textview : TextView = findViewById(R.id.textViewResult)
+
+        val btn0 : Button = findViewById(R.id.button0)
+        val btn1 : Button = findViewById(R.id.button1)
+        val btn2 : Button = findViewById(R.id.button2)
+        val btn3 : Button = findViewById(R.id.button3)
+        val btn4 : Button = findViewById(R.id.button4)
+        val btn5 : Button = findViewById(R.id.button5)
+        val btn6 : Button = findViewById(R.id.button6)
+        val btn7 : Button = findViewById(R.id.button7)
+        val btn8 : Button = findViewById(R.id.button8)
+        val btn9 : Button = findViewById(R.id.button9)
+        val btnAdd : Button = findViewById(R.id.buttonAdd)
+        val btnClear : Button = findViewById(R.id.buttonClear)
+        val btnDiv : Button = findViewById(R.id.buttonDiv)
+        val btnEqual : Button = findViewById(R.id.buttonEqual)
+        val btnSub : Button = findViewById(R.id.buttonSub)
+        val btnMul : Button = findViewById(R.id.buttonMul)
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
